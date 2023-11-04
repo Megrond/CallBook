393b3f10e51ca95fa61b6bcfa592921514b7ec07
Витёк Ромашов <romas-viktor@yandex.ru>
2023-11-04 11:25:34 +0300

    +

--- CallBook/main.cpp
+++ CallBook/main.cpp
@@ -11,7 +11,7 @@ int main()
 	setlocale(LC_ALL, "RU");
 
 	Callbook user_one("Romashov V.V.", 84843925555, 84952396548, 89190390232, "Just a good guy!");
-	Callbook user_two("Odegov A.N.", 84843925555, 84952396548, 89190390232, "Wonderful person!");
+	Callbook user_two("Odegov A.N.", 84843969425, 84955623487, 89602223355, "Wonderful person!");
 
 	user_one.showBook();
 	user_two.showBook();
